fa300ab4367d2823e1f8b32c8226b86e0a7ae4dd
onishi taisei <taisei.developer@gmail.com>
2024-01-24 00:13:32 +0900

    🚀 feat: Add extension functions for grouping multiple AsyncValues

--- lib/multi_async_value.dart
+++ lib/multi_async_value.dart
@@ -1,7 +1,96 @@
 library multi_async_value;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:riverpod/riverpod.dart';
+
+extension MultiAsyncValue on AsyncValue {
+  static AsyncValue<(T1, T2)> group2<T1, T2>(
+    AsyncValue<T1> t1,
+    AsyncValue<T2> t2,
+  ) {
+    if (t1 is AsyncLoading || t2 is AsyncLoading) {
+      return const AsyncLoading();
+    }
+
+    try {
+      return AsyncData((
+        t1.value as T1,
+        t2.value as T2,
+      ));
+    } catch (e, st) {
+      return AsyncError(e, st);
+    }
+  }
+
+  static AsyncValue<(T1, T2, T3)> group3<T1, T2, T3>(
+    AsyncValue<T1> t1,
+    AsyncValue<T2> t2,
+    AsyncValue<T3> t3,
+  ) {
+    if (t1 is AsyncLoading || t2 is AsyncLoading || t3 is AsyncLoading) {
+      return const AsyncLoading();
+    }
+
+    try {
+      return AsyncData((
+        t1.value as T1,
+        t2.value as T2,
+        t3.value as T3,
+      ));
+    } catch (e, st) {
+      return AsyncError(e, st);
+    }
+  }
+
+  static AsyncValue<(T1, T2, T3, T4)> group4<T1, T2, T3, T4>(
+    AsyncValue<T1> t1,
+    AsyncValue<T2> t2,
+    AsyncValue<T3> t3,
+    AsyncValue<T4> t4,
+  ) {
+    if (t1 is AsyncLoading ||
+        t2 is AsyncLoading ||
+        t3 is AsyncLoading ||
+        t4 is AsyncLoading) {
+      return const AsyncLoading();
+    }
+
+    try {
+      return AsyncData((
+        t1.value as T1,
+        t2.value as T2,
+        t3.value as T3,
+        t4.value as T4,
+      ));
+    } catch (e, st) {
+      return AsyncError(e, st);
+    }
+  }
+
+  static AsyncValue<(T1, T2, T3, T4, T5)> group5<T1, T2, T3, T4, T5>(
+    AsyncValue<T1> t1,
+    AsyncValue<T2> t2,
+    AsyncValue<T3> t3,
+    AsyncValue<T4> t4,
+    AsyncValue<T5> t5,
+  ) {
+    if (t1 is AsyncLoading ||
+        t2 is AsyncLoading ||
+        t3 is AsyncLoading ||
+        t4 is AsyncLoading ||
+        t5 is AsyncLoading) {
+      return const AsyncLoading();
+    }
+
+    try {
+      return AsyncData((
+        t1.value as T1,
+        t2.value as T2,
+        t3.value as T3,
+        t4.value as T4,
+        t5.value as T5,
+      ));
+    } catch (e, st) {
+      return AsyncError(e, st);
+    }
+  }
 }
